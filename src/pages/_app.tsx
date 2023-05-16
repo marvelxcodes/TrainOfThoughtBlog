163d76b51765e298225f59543a813faf0a65543e
marvelxcodes <ramakrishnan0046@gmail.com>
2023-05-16 17:38:51 +0530

    Added Components to Layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Contact from '@/components/Contact';
+import Footer from '@/components/Footer';
+import Header from '@/components/Header/Header';
+import '@/styles/globals.scss';
+import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => {
+	return (
+		<>
+			<Header />
+			<Component {...pageProps} />
+			<Contact />
+			<Footer />
+		</>
+	);
+};
+
+export default App;
