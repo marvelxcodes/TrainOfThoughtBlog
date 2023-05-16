a8ac6c839a20806b2364766866d670c38f20a3c0
marvelxcodes <ramakrishnan0046@gmail.com>
2023-05-16 17:40:08 +0530

    Added Footer Component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+	return (
+		<footer className='flex justify-center p-5 bg-gray-300 text-center'>
+			© 2023 by Train of Thoughts. Powered By MarvelXCodes
+		</footer>
+	);
+};
+
+export default Footer;
