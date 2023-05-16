8881971c9706a5b7e6e1cf11013d712d5f5d9510
marvelxcodes <ramakrishnan0046@gmail.com>
2023-05-16 17:43:04 +0530

    Finished Structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,130 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from 'next/image';
+import { Inter, Playfair_Display, Heebo } from 'next/font/google';
+import LocalFont from 'next/font/local';
+import styles from '@/styles/Home.module.scss';
+import Time from '@/components/Time';
+import Subscribe from '@/components/Subscribe';
+import Button from '@/components/Button';
+import Link from 'next/link';
+import H3 from '@/components/H3';
+import {
+	FacebookIcon,
+	InstagramIcon,
+	LinkedInIcon,
+	TwitterIcon,
+} from '@/components/Icons';
 
-const inter = Inter({ subsets: ['latin'] })
+const playfair_display = Playfair_Display({
+	subsets: ['latin'],
+	weight: '700',
+});
+const avenir = LocalFont({
+	src: '../fonts/Avenir.otf',
+});
 
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const testData = {
+		id: 1,
+		title: "Back to Fiction: What I'm Reading This Summer",
+		subtitle:
+			'Create a blog post subtitle that summarizes your post in a few short, punchy sentences and entices your audience to continue reading...',
+		postedOn: new Date(),
+		readingTime: 2,
+		thumbnail: '/assets/featured_post.jpg',
+		views: 100,
+		comments: {
+			length: 1,
+		},
+		heart: 5,
+		content: 'Hello World',
+	};
+	return (
+		<main className={styles.container}>
+			<div className={styles.section1}>
+				{/* Featured Post */}
+				<div className={styles.featuredPostContainer}>
+					<h3 style={avenir.style} className={styles.headText}>
+						FEATURED POST
+					</h3>
+					<Image
+						className={styles.thumbnail}
+						alt='book in hand'
+						width={720}
+						height={1280}
+						src={testData.thumbnail}
+					/>
+					<div className='p-5 space-y-3'>
+						<Time
+							readingTime={testData.readingTime}
+							postedOn={testData.postedOn}
+						/>
+						<h3 className='text-3xl max-md:2xl font-bold'>
+							{testData.title}
+						</h3>
+						<h4 className='text-lg max-md:text-base'>
+							{testData.subtitle}
+						</h4>
+					</div>
+				</div>
+			</div>
+			<Subscribe />
+			<div className='flex max-md:flex-col my-5'>
+				<section className='border-r border-black flex-1'>
+					<div className='mx-auto max-w-xl'>
+						<H3>TRAIN OF THOUGHT</H3>
+						<Link href='/blog'>
+							<Button>More Posts</Button>
+						</Link>
+					</div>
+				</section>
+				<section className='flex-1'>
+					<div className='mx-auto max-w-md flex flex-col gap-6'>
+						<H3>ABOUT ME</H3>
+						<Image />
+						<p className='leading-7 tracking-wide'>
+							Hey There! I&apos;m Rama Krishnan V, a Fasinated Teenage
+							Full Stack Web Developer and a Tech Enthusiast from South
+							India. In my pastime, I write some articles about
+							technologies like React, Typescript, svelte and many
+							more...
+						</p>
+						<Link
+							className='hover:text-[var(--color-accent)]'
+							href='/about'
+						>{`Read More >`}</Link>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+						<hr className='' />
+						<H3>FOLLOW ME</H3>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+						<div className='border-y border-black py-5 justify-center flex gap-x-6'>
+							<Link href='https://linkedin.com/in/marvelxcodes'>
+								<LinkedInIcon className='h-6 w-6' />
+							</Link>
+							<Link href='https://facebook.com/marvelxcodes'>
+								<FacebookIcon className='h-6 w-6' />
+							</Link>
+							<Link href='https://instagram.com/marvelxcodes'>
+								<InstagramIcon className='h-6 w-6' />
+							</Link>
+							<Link href='https://twitter.com/marvelxcodes'>
+								<TwitterIcon className='h-6 w-6' />
+							</Link>
+						</div>
+						<H3>SUBSCRIBE</H3>
+						<form>
+							<label>
+								Enter your email *
+								<input type='text' />
+							</label>
+							<Button type='submit'>Subscribe</Button>
+							<hr className='' />
+							<H3>MY PICK OF THE MONTH</H3>
+							<Image className='outline' />
+							<p></p>
+						</form>
+					</div>
+				</section>
+			</div>
+		</main>
+	);
 }
